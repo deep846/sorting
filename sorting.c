a99b5acdf3b751cdec330988972a3008722ed078
deep846 <45318452+deep846@users.noreply.github.com>
2020-10-08 14:28:44 +0530

    sorting

---
+++ sorting.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+
+int sorting(int *x,int n)
+{
+    int i,j,t;
+    for(i=0; i<n; i++)
+    {
+        for(j=i; j<n; j++)
+        {
+            if(x[i]>x[j])
+            {
+                t=
+    {
+        printf("\n Enter the %d array element = ",i);
+        scanf("%d",&x[i]);
+    }
+    
+    printf("\n\n Before sorting array elements are :- ");
+    for(i=0; i<n; i++)
+    {
+        printf("  %d",x[i]);
+    }
+    sorting(x,n);
+    printf("\n\n After sorting array elements are :- ");
+    for(i=0; i<n; i++)
+    {
+        printf("  %d",x[i]);
+    }
+    
+    
+}
